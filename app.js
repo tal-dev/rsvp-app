9d890874ec8e4bf3f411ff332e40a78040c5e5e4
tal-dev <talgat.abykeev@gmail.com>
2020-05-06 21:45:48 -0500

    added submit and remove functionality

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const form = document.querySelector('#registrar');
 const input = form.querySelector('input');
+const ul = document.querySelector('#invitedList');
 
  form.addEventListener('submit', (e) => {
+
      e.preventDefault();
      const text = input.value;
-     const ul = document.querySelector('#invitedList');
+     input.value = '';
+
      const li = document.createElement('li');
      ul.appendChild(li);
      li.textContent = text;
-     input.value = '';
+
+     let label = document.createElement('label');
+     label.textContent = 'Confirmed';
+     let checkbox = document.createElement('input');
+     checkbox.type = 'checkbox';
+     label.appendChild(checkbox);
+     li.appendChild(label);
+
+     let removeButton = document.createElement('button');
+     removeButton.textContent = 'remove';
+     label.appendChild(removeButton);
+
+     ul.appendChild(li);
  });
 
+ ul.addEventListener('change', (e) => {
+     let checkbox = e.target;
+     let checked = checkbox.checked;
+     let listItem = checkbox.parentNode.parentNode;
+
+     if(checked) {
+        listItem.className = 'responded';
+     }
+     else {
+         listItem.className = '';
+     }
+ });
   
 
 
